0a15572bde7bbf984244555b3ae9d9ac45981152
ElenaMUSCEL <124464514+ElenaMUSCEL@users.noreply.github.com>
2023-02-09 21:22:31 +0200

    Update

--- src/Feb9.java
+++ src/Feb9.java
@@ -1,5 +1,14 @@
 public class Feb9 {
     public static void main(String[] args) {
+        int counter = 0;
 
+        for(int i = 0; i < 5; i++){
+            counter++;
+            
+            if(counter >= 3){
+                System.out.println("user is not allowd to...");
+            }
+
+     }
     }
 }
